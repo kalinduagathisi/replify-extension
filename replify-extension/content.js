04cf1cba446e9fe5a7c143ff932cfb3f1f10e73f
kalinduagathisi <kalinduagathisi@gmail.com>
2025-02-20 07:33:09 +0530

    added mutationObserver to detect compose window loading

--- replify-extension/content.js
+++ replify-extension/content.js
@@ -1,1 +1,24 @@
-console.log("Replify - Content Script Loaded")
+console.log("Replify - Content Script Loaded");
+
+function injectButton() {}
+
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    const addedNodes = Array.from(mutation.addedNodes);
+    const hasComposeElements = addedNodes.some(
+      (node) =>
+        node.nodeType === Node.ELEMENT_NODE &&
+        (node.matches('.aDh, .btC, [role="dialog"]') ||
+          node.querySelector('.aDh, .btC, [role="dialog"]'))
+    );
+    if (hasComposeElements) {
+      console.log("Compose window detected");
+      setTimeout(injectButton, 500);
+    }
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
