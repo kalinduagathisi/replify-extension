2fb03af8c6358e7563c02f53aa9e7fa204d3e2a0
kalinduagathisi <kalinduagathisi@gmail.com>
2025-02-23 08:11:39 +0530

    implemented find compose toolbar btn and inject reply btn

--- replify-extension/content.js
+++ replify-extension/content.js
@@ -1,6 +1,44 @@
 console.log("Replify - Content Script Loaded");
 
-function injectButton() {}
+function createAIButton() {
+    
+
+}
+
+function findComposeToolBar() {
+    const selectors = [
+        'btC',
+        'aDh',
+        '[role="toolbar"]',
+        '.gU.Up'
+    ];
+    for (const selector of selectors) {
+        const toolbar = document.querySelector(selector);
+        if (toolbar) {
+            return toolbar;
+        }
+        return null;
+    }
+}
+
+function injectButton() {
+    const existingButton = document.querySelector('.ai-reply-btn');
+    if(existingButton) existingButton.remove();
+
+    const toolbar = findComposeToolBar();
+    if (!toolbar) {
+        console.log("Toolbar not found!")
+        return;
+    }
+
+    console.log("Toolbar found, Creating AI Button");
+    const button = createAIButton();
+    button.classList.add(".ai-reply-btn");
+    button.addEventListener('click', async () => {
+
+    });
+    toolbar.insertBefore(button, toolbar.firstChild);
+}
 
 const observer = new MutationObserver((mutations) => {
   for (const mutation of mutations) {
